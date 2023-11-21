d65456373849c510751e9f67bd54aef0d111c6be
Ulan Degenbaev <ulan.degenbaev@dfinity.org>
2023-11-21 14:05:55 +0000

    Implement the canister

---
+++ README.md
@@ -0,0 +1,32 @@
+# ic-config-canister
+
+This canister stores `(replica-version, json-config)` pairs and allows to query them via HTTP.
+
+This canister was build with Azle. In order to install Azle, run the following command from the root of the repository:
+```
+npm install
+```
+
+Assuming that `dfx` has been started with `dfx start` and is running, you deploy the canister locally with:
+```
+dfx deploy
+```
+
+In order to add `(replica-version, json-config)` use the following command:
+```
+dfx canister call ic-config-canister add '("version_foo", "{\"config_bar\":1}")'
+```
+Note that the quotes should be properly escaped in the JSON config.
+
+
+You can query the list of stored versions by making a GET HTTP request to the `/versions` endpoint.
+```
+curl "http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943/versions"
+```
+Replace the URL prefix with your local canister id.
+
+You can query the config of a particular replica version by requesting `/config?version=version_foo` endpoint.
+```
+curl "http://be2us-64aaa-aaaaa-qaabq-cai.localhost:4943/config?version=version_foo"
+```
+Replace the URL prefix with your local canister id.
---
+++ src/index.ts
@@ -0,0 +1,101 @@
+import {
+    blob,
+    Canister,
+    init,
+    nat16,
+    Opt,
+    query,
+    Record,
+    StableBTreeMap,
+    text,
+    Tuple,
+    update,
+    Vec
+} from 'azle';
+
+
+type HeaderField = [text, text];
+const HeaderField = Tuple(text, text);
+
+const HttpResponse = Record({
+    status_code: nat16,
+    headers: Vec(HeaderField),
+    body: blob,
+});
+
+const HttpRequest = Record({
+    method: text,
+    url: text,
+    headers: Vec(HeaderField),
+    body: blob,
+    certificate_version: Opt(nat16)
+});
+
+let configByReplicaVersion = StableBTreeMap<text, text>(text, text, 0);
+
+export default Canister({
+    init: init([], () => { }),
+    http_request: query([HttpRequest], HttpResponse, (req) => {
+        if (req.method != 'GET') {
+            return error('Only GET requests are supported');
+        }
+
+        if (req.url.startsWith('/versions')) {
+            const keys = configByReplicaVersion.keys();
+            return {
+                status_code: 200,
+                headers: [['content-type', 'application/json']],
+                body: encode(JSON.stringify(keys)),
+            };
+        }
+
+        if (req.url.startsWith('/config')) {
+            const match = /version=([^&]+)/g.exec(req.url);
+            if (!match || !match[1]) {
+                return error('Expected a version argument: ?version=<replica_version>');
+            }
+            const version = match[1];
+            const maybeConfig = configByReplicaVersion.get(version);
+            if (!maybeConfig.Some) {
+                return error(`No config for the given version: ${version}`);
+            }
+            const config = maybeConfig.Some;
+            return {
+                status_code: 200,
+                headers: [['content-type', 'application/json']],
+                body: encode(config),
+            };
+        }
+
+        return {
+            status_code: 404,
+            headers: [['content-type', 'application/json']],
+            body: encode('Supported endpoints: /versions and /config'),
+        };
+
+    }),
+
+    add: update([text, text], text, (version, config) => {
+        console.log("received " + config);
+        try {
+            JSON.parse(config);
+        } catch (err) {
+            return `The given config is not a valid JSON: ${err}`;
+        }
+        configByReplicaVersion.insert(version, config);
+        return `Added config for version ${version}`;
+    }),
+});
+
+
+function encode(string: string): blob {
+    return Buffer.from(string, 'utf-8');
+}
+
+function error(msg: text): typeof HttpResponse {
+    return {
+        status_code: 400,
+        headers: [],
+        body: encode(msg),
+    };
+}
